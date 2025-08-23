ddab498455f4f75def0beb04846a10d9ce17ca1e
raytchel123 <crispim@raytchel.com>
2025-08-23 10:32:01 -0300

    Updated UsuariosPage.tsx

--- src/pages/UsuariosPage.tsx
+++ src/pages/UsuariosPage.tsx
@@ -266,6 +266,9 @@ export function UsuariosPage() {
                     Pode Aprovar
                   </th>
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Visualização
+                  </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     Criado em
                   </th>
                   <th className="px-6 py-3 text-right text-xs font-medium text-gray-500 uppercase tracking-wider">
