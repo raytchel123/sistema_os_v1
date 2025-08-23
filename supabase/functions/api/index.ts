22e50b2b721474a756c47baa87850c9b33cfd5af
raytchel123 <crispim@raytchel.com>
2025-08-23 11:23:53 -0300

    Updated index.ts

--- supabase/functions/api/index.ts
+++ supabase/functions/api/index.ts
@@ -1264,7 +1264,6 @@ Deno.serve(async (req) => {
         atualizado_em: new Date().toISOString()
       };
 
-      conste.log(body)
       // Add password if provided
       if (body.senha) {
         const passwordHash = await crypto.subtle.digest(
